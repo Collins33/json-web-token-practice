8e15507054ece0b691a3587126cfd62f5fa99f41
collins njau <collinsnjau39@gmail.com>
2018-11-12 15:48:36 +0300

    Generate the token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const jwt = require('jsonwebtoken')
 const port =3000
 
 // use the body parser middleware to process json
@@ -12,12 +13,35 @@ app.get('/', (req,res)=>{
     });
 });
 
+const users = []
+
 app.post('/login', (req,res) =>{
+    if (!req.body.username || !req.body.email){
+        res.status(400).send({
+            message:"invalid request body"
+        });
+        return;
+    }
     const username= req.body.username
     const email = req.body.email
+    const user = {
+        "username":username,
+        "email":email
+    }
+    users.push(user)
+    // generate token when logging in a user
+    const token = jwt.sign({
+        sub:user.username,
+        email:user.email
+    }, "myKey", {expiresIn: "3 hours"})
     res.status(201).send({
-        message:`hello ${username}`
+        message:`hello ${username}`,
+        token: token
     });
+})
+
+app.get('/user', (req,res) =>{
+    res.status(200).send(users)
 })
 
 app.get("*", (req,res) =>{
