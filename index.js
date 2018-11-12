cff98cadffe7f475995b394f03a1db75eb5d5dca
collins njau <collinsnjau39@gmail.com>
2018-11-12 15:28:15 +0300

    Add middleware to process json

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port =3000
+
+// use the body parser middleware to process json
+app.use(bodyParser.json())
 
 app.get('/', (req,res)=>{
     res.status(200).send({
         message:"Welcome earthling"
     });
 });
+
+app.post('/login', (req,res) =>{
+    const username= req.body.username
+    const email = req.body.email
+    res.status(201).send({
+        message:`hello ${username}`
+    });
+})
 
 app.get("*", (req,res) =>{
     res.status(404).send({
